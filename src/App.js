28c9657833f76141ebcfa6fda841804532f88dfe
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-08-08 22:37:24 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>tran quang thong</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
